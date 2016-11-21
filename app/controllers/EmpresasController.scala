2e855f94efb6525368fa0538f127f46a646c695f
osocron <osocron@gmail.com>
2016-11-21 16:56:00 -0600

    Like Yeah!

--- app/controllers/EmpresasController.scala
+++ app/controllers/EmpresasController.scala
@@ -4,11 +4,16 @@ import javax.inject.Inject
 
 import io.circe.{Decoder, Encoder}
 import io.circe.generic.semiauto._
+import io.circe.generic.auto._
+import io.circe.syntax._
 import model.entities.Tables.{Empresas, EmpresasRow}
 import model.repositories.EmpresasRepo
 import play.api.libs.circe.Circe
 import play.api.mvc._
 import slick.driver.MySQLDriver.api._
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by osocron on 20/11/16.
@@ -26,6 +31,15 @@ class EmpresasController @Inject()(empresasRepo: EmpresasRepo)
 
   def empresa(idEmpresa: Int) = Action.async { request =>
     queryAction(empresasRepo, "Empresa no encontrada")(_.idEmpresa === idEmpresa)
+  }
+
+  def empresaPorOferta(idOferta: Int) = Action.async { request =>
+    empresasRepo.getEmpresaPorOferta(idOferta).flatMap {
+      case Some(e) => Future.successful(Ok(e.asJson.noSpaces))
+      case None   =>  Future.successful(
+        Ok(Message(error = false, "Empresa no encontrada").asJson.noSpaces)
+      )
+    }
   }
 
 }
--- app/model/repositories/EmpresasRepo.scala
+++ app/model/repositories/EmpresasRepo.scala
@@ -2,14 +2,26 @@ package model.repositories
 
 import javax.inject.Inject
 
-import model.entities.Tables.{Empresas, EmpresasRow}
+import model.entities.Tables._
 import play.api.db.slick.DatabaseConfigProvider
 import slick.lifted.TableQuery
+import slick.driver.MySQLDriver.api._
+
+import scala.concurrent.Future
 
 /**
   * Created by osocron on 19/11/16.
   */
 class EmpresasRepo @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)
   extends RepositoryUtils[Empresas, EmpresasRow] {
+
   override val t: TableQuery[Empresas] = TableQuery[Empresas]
+
+  def getEmpresaPorOferta(idOferta: Int): Future[Option[EmpresasRow]] = {
+    db.run((for {
+      oferta  <- Ofertas if oferta.idOferta === idOferta
+      empresa <- Empresas if empresa.idEmpresa === oferta.idOferta
+    } yield empresa).result.headOption)
+  }
+
 }
