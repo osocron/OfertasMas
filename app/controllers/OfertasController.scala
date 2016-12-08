ddf0c31ee1797c91f40486783263dcb27adbe08d
osocron <osocron@gmail.com>
2016-12-07 19:04:18 -0600

    Added Login para Usuario

--- app/controllers/OfertasController.scala
+++ app/controllers/OfertasController.scala
@@ -6,6 +6,7 @@ import io.circe.{Decoder, Encoder}
 import io.circe.generic.semiauto._
 import io.circe.generic.auto._
 import io.circe.syntax._
+import model.entities.Tables
 import model.entities.Tables.{Ofertas, OfertasRow}
 import model.repositories.OfertasRepo
 import play.api.libs.circe.Circe
@@ -13,6 +14,7 @@ import play.api.mvc._
 import slick.driver.MySQLDriver.api._
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 /**
   * Created by osocron on 20/11/16.
@@ -24,28 +26,28 @@ class OfertasController @Inject()(ofertasRepo: OfertasRepo)
 
   override implicit val decoder: Decoder[OfertasRow] = deriveDecoder[OfertasRow]
 
-  def ofertas: Action[AnyContent] = Action.async { request =>
+  def ofertas: Action[AnyContent] = Action.async { _ =>
     getAction(ofertasRepo)
   }
 
-  def oferta(idOferta: Int): Action[AnyContent] = Action.async { request =>
+  def oferta(idOferta: Int): Action[AnyContent] = Action.async { _ =>
     queryAction(ofertasRepo, "Oferta no encontrada")(_.idOferta === idOferta)
   }
 
-  def ofertaPorNombre(nombreOferta: String): Action[AnyContent] = Action.async { request =>
+  def ofertaPorNombre(nombreOferta: String): Action[AnyContent] = Action.async { _ =>
     queryAction(ofertasRepo, "Oferta no encontrada")(_.nombreOferta === nombreOferta)
   }
 
-  def ofertasPorCategorias(nombreCategoria: String): Action[AnyContent] = Action.async { request =>
-    (for {
-      ofertas <- ofertasRepo.getOfertasPorCategoria(nombreCategoria)
-    } yield Ok(ofertas.asJson.noSpaces))
-      .recover { case cause => Ok(Message(error = true, cause.getMessage).asJson.noSpaces) }
-  }
+  def ofertasPorCategorias(nombreCategoria: String): Action[AnyContent] =
+    ofertasByFunction(ofertasRepo.getOfertasPorCategoria(nombreCategoria))
 
-  def ofertasPorCiudadCategoria(idCiudad: Int, idCategoria: Int): Action[AnyContent] = Action.async { request =>
+  def ofertasPorCiudadCategoria(idCiudad: Int, idCategoria: Int): Action[AnyContent] =
+    ofertasByFunction(ofertasRepo.getOfertasPorCiudadCategoria(idCiudad, idCategoria))
+
+
+  def ofertasByFunction(b: => Future[Seq[Tables.OfertasRow]]): Action[AnyContent] = Action.async { _ =>
     (for {
-      ofertas <- ofertasRepo.getOfertasPorCiudadCategoria(idCiudad, idCategoria)
+      ofertas <- b
     } yield Ok(ofertas.asJson.noSpaces))
       .recover { case cause => Ok(Message(error = true, cause.getMessage).asJson.noSpaces) }
   }
--- app/controllers/UsuarioController.scala
+++ app/controllers/UsuarioController.scala
@@ -4,11 +4,15 @@ import javax.inject.Inject
 
 import io.circe.{Decoder, Encoder}
 import io.circe.generic.semiauto._
+import io.circe.generic.auto._
+import io.circe.syntax._
 import model.entities.Tables.{Usuario, UsuarioRow}
 import model.repositories.UsuarioRepo
 import play.api.libs.circe.Circe
 import play.api.mvc._
 import slick.driver.MySQLDriver.api._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by osocron on 20/11/16.
@@ -20,16 +24,28 @@ class UsuarioController @Inject()(usuarioRepo: UsuarioRepo)
 
   override implicit val decoder: Decoder[UsuarioRow] = deriveDecoder[UsuarioRow]
 
-  def usuarios: Action[AnyContent] = Action.async { request =>
+  def usuarios: Action[AnyContent] = Action.async { _ =>
     getAction(usuarioRepo)
   }
 
-  def usuario(correoUsuario: String): Action[AnyContent] = Action.async { request =>
+  def usuario(correoUsuario: String): Action[AnyContent] = Action.async { _ =>
     queryAction(usuarioRepo, "Usuario no encontrado")(_.correoUsuario === correoUsuario)
   }
 
   def nuevoUsuario: Action[UsuarioRow] = Action.async(circe.json[UsuarioRow]) { request =>
     insertAction(request, usuarioRepo)
+  }
+
+  def login: Action[AnyContent] = Action.async { request =>
+    val data = request.body.asFormUrlEncoded
+    (for {
+      correo <- data.get("correo").map(_.toString)
+      password <- data.get("contrasena").map(_.toString)
+    } yield for {
+      b <- usuarioRepo.queryIfExists(u => u.correoUsuario === correo && u.contrasenaUsuario === password)
+    } yield
+      if (b) Ok(Message(error = false, "Encontrado").asJson.noSpaces)
+      else Ok(Message(error = true, "Usuadio no encontrado").asJson.noSpaces)).head
   }
 
 }
